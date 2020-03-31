bf8fc37ed9bdf34b4136fb4608efed74554ca683
IanCouto <48738352+IanCouto@users.noreply.github.com>
2020-03-31 11:54:19 -0300

    crian novo construtor e maneira de padronizar tamanho de strings e vetores

--- src/main/java/Leitura.java
+++ src/main/java/Leitura.java
@@ -5,9 +5,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-
-
-
 import Livro.Livro;
 
 public class Leitura {
--- src/main/java/Livro/Livro.java
+++ src/main/java/Livro/Livro.java
@@ -8,20 +8,18 @@ import java.util.List;
 /**
  *
  */
-public class Livro implements Serializable{
-    
+public class Livro implements Serializable {
+
     private int rank; // ranking na lista de mais vendidos
-    private int categorie; //* lista de ids de categorias de livros (os nomes das categorias estão em categories.csv)
-    private int id; // *id único do livro atribuído pelo Bookdepository.com
-    private int publicationPlace; // *id do local de publicação do livro(os locas de publicação estão em places.csv) 
+    private int id; // id único do livro atribuído pelo Bookdepository.com 
     private int ratingsAVG; // avaliação média 0-5
     private int ratingsCount; // número de avaliações   
-            
+
     private float dimensionX; // largura cm
     private float dimensionY; // altura cm
     private float dimensionZ; // espessura mm
     private float weight; // peso
-    
+
     private String edition; // edição
     private String editionStatment; // informação de edição
     private String description; //descrição do livro
@@ -32,383 +30,181 @@ public class Livro implements Serializable{
     private String isbn13; // ISBN-13
     private String publisher; // editora
     private String title; // título
+    private String publicationPlace; // *id do local de publicação do livro(os locas de publicação estão em places.csv)
+   
+    private String[] categorie; //* lista de ids de categorias de livros (os nomes das categorias estão em categories.csv)
+    private String[] author; //*vetor de ids de autores entre colchetes e separados por vírgulas (os nomes dos autores associados aos ids estão em authors.csv)
+    private String[] format; //* id do formato do livro(os formatos possiveis estão em formats.csv)
+    private String[] lang; // vetor de idiomas do livro
     
-    private List<String> lang; // lista de idiomas do livro
-    private List<Integer> author; //lista de ids de autores entre colchetes e separados por vírgulas (os nomes dos autores associados aos ids estão em authors.csv)
-    private List<Integer> format; //* id do formato do livro(os formatos possiveis estão em formats.csv)
-    
+
     private Date indexDate; // data de indexação
     private Date publicationDate; // data de publicação
-       
+
     private URL url; // url relativa (https://bookdepository.com + url)
-    
-    public Livro(){
-        
-    }
 
-    /**
-     * @return the rank
-     */
-    public int getRank() {
-        return rank;
-    }
-
-    /**
-     * @param rank the rank to set
-     */
-    public void setRank(int rank) {
+    public Livro(int rank, int id, int ratingsAVG, int ratingsCount, float dimensionX, float dimensionY, 
+            float dimensionZ, float weight, String edition, String editionStatment, String description, 
+            String forAges, String illustrationNotes, String imprint, String isbn10, String isbn13,
+            String publisher, String title,String publicationPlace, String[] categorie,
+            String[] author, String[]format,String[] lang, Date indexDate, Date publicationDate, URL url)
+    {
         this.rank = rank;
-    }
-
-    /**
-     * @return the categorie
-     */
-    public int getCategorie() {
-        return categorie;
-    }
-
-    /**
-     * @param categorie the categorie to set
-     */
-    public void setCategorie(int categorie) {
-        this.categorie = categorie;
-    }
-
-
-    /**
-     * @return the id
-     */
-    public int getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(int id) {
         this.id = id;
-    }
-
-    /**
-     * @return the publicationPlace
-     */
-    public int getPublicationPlace() {
-        return publicationPlace;
-    }
-
-    /**
-     * @param publicationPlace the publicationPlace to set
-     */
-    public void setPublicationPlace(int publicationPlace) {
-        this.publicationPlace = publicationPlace;
-    }
-
-    /**
-     * @return the ratingsAVG
-     */
-    public int getRatingsAVG() {
-        return ratingsAVG;
-    }
-
-    /**
-     * @param ratingsAVG the ratingsAVG to set
-     */
-    public void setRatingsAVG(int ratingsAVG) {
         this.ratingsAVG = ratingsAVG;
-    }
-
-    /**
-     * @return the ratingsCount
-     */
-    public int getRatingsCount() {
-        return ratingsCount;
-    }
-
-    /**
-     * @param ratingsCount the ratingsCount to set
-     */
-    public void setRatingsCount(int ratingsCount) {
         this.ratingsCount = ratingsCount;
-    }
-
-    /**
-     * @return the dimensionX
-     */
-    public float getDimensionX() {
-        return dimensionX;
-    }
-
-    /**
-     * @param dimensionX the dimensionX to set
-     */
-    public void setDimensionX(float dimensionX) {
+        //========================
         this.dimensionX = dimensionX;
-    }
-
-    /**
-     * @return the dimensionY
-     */
-    public float getDimensionY() {
-        return dimensionY;
-    }
-
-    /**
-     * @param dimensionY the dimensionY to set
-     */
-    public void setDimensionY(float dimensionY) {
         this.dimensionY = dimensionY;
-    }
-
-    /**
-     * @return the dimensionZ
-     */
-    public float getDimensionZ() {
-        return dimensionZ;
-    }
-
-    /**
-     * @param dimensionZ the dimensionZ to set
-     */
-    public void setDimensionZ(float dimensionZ) {
         this.dimensionZ = dimensionZ;
-    }
-
-    /**
-     * @return the weight
-     */
-    public float getWeight() {
-        return weight;
-    }
-
-    /**
-     * @param weight the weight to set
-     */
-    public void setWeight(float weight) {
         this.weight = weight;
-    }
-
-    /**
-     * @return the edition
-     */
-    public String getEdition() {
-        return edition;
-    }
-
-    /**
-     * @param edition the edition to set
-     */
-    public void setEdition(String edition) {
+        //========================
         this.edition = edition;
-    }
-
-    /**
-     * @return the editionStatment
-     */
-    public String getEditionStatment() {
-        return editionStatment;
-    }
-
-    /**
-     * @param editionStatment the editionStatment to set
-     */
-    public void setEditionStatment(String editionStatment) {
         this.editionStatment = editionStatment;
-    }
-
-
-    /**
-     * @return the description
-     */
-    public String getDescription() {
-        return description;
-    }
-
-    /**
-     * @param description the description to set
-     */
-    public void setDescription(String description) {
         this.description = description;
-    }
-
-    /**
-     * @return the forAges
-     */
-    public String getForAges() {
-        return forAges;
-    }
-
-    /**
-     * @param forAges the forAges to set
-     */
-    public void setForAges(String forAges) {
         this.forAges = forAges;
-    }
-
-    /**
-     * @return the illustrationsNote
-     */
-    public String getIllustrationsNote() {
-        return illustrationsNote;
-    }
-
-    /**
-     * @param illustrationsNote the illustrationsNote to set
-     */
-    public void setIllustrationsNote(String illustrationsNote) {
-        this.illustrationsNote = illustrationsNote;
-    }
-
-    /**
-     * @return the imprint
-     */
-    public String getImprint() {
-        return imprint;
-    }
-
-    /**
-     * @param imprint the imprint to set
-     */
-    public void setImprint(String imprint) {
+        this.illustrationsNote = illustrationNotes;
         this.imprint = imprint;
-    }
-
-    /**
-     * @return the isbn10
-     */
-    public String getIsbn10() {
-        return isbn10;
-    }
-
-    /**
-     * @param isbn10 the isbn10 to set
-     */
-    public void setIsbn10(String isbn10) {
         this.isbn10 = isbn10;
-    }
-
-    /**
-     * @return the isbn13
-     */
-    public String getIsbn13() {
-        return isbn13;
-    }
-
-    /**
-     * @param isbn13 the isbn13 to set
-     */
-    public void setIsbn13(String isbn13) {
         this.isbn13 = isbn13;
-    }
-
-    /**
-     * @return the publisher
-     */
-    public String getPublisher() {
-        return publisher;
-    }
-
-    /**
-     * @param publisher the publisher to set
-     */
-    public void setPublisher(String publisher) {
         this.publisher = publisher;
+        this.title = title;
+        this.publicationPlace = publicationPlace;
+        //========================
+        this.categorie = categorie;
+        this.author = author;
+        this.format = format;
+        this.lang = lang;
+        //========================
+        this.indexDate = indexDate;
+        this.publicationDate = publicationDate;
+        this.url = url;
     }
 
-    /**
-     * @return the title
-     */
+    public Livro() {
+
+    }
+    
+    public String preencheString(String str, int tamMax){
+        if(str.length() > tamMax){
+            return str.substring(0, tamMax);
+        }else if(str.length() < tamMax){
+            String aux = new String();
+            aux = str;
+            for (int i = aux.length(); i < tamMax; i++) {
+                aux.concat("*");//preenche a string com "*" caso a string passada seja menor que o tamanho maximo
+            }
+            return aux;
+        }else{
+            return str;
+        }
+    }
+    
     public String getTitle() {
         return title;
     }
 
-    /**
-     * @param title the title to set
-     */
     public void setTitle(String title) {
         this.title = title;
     }
 
-    /**
-     * @return the lang
-     */
-    public List<String> getLang() {
-        return lang;
+    public String[] getAuthor() {
+        return author;
     }
 
-    /**
-     * @param lang the lang to set
-     */
-    public void setLang(List<String> lang) {
-        this.lang = lang;
+    public String[] getCategorie() {
+        return categorie;
     }
 
-    /**
-     * @return the indexDate
-     */
+    public String getDescription() {
+        return description;
+    }
+
+    public float getDimensionX() {
+        return dimensionX;
+    }
+
+    public float getDimensionY() {
+        return dimensionY;
+    }
+
+    public float getDimensionZ() {
+        return dimensionZ;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public String getEditionStatment() {
+        return editionStatment;
+    }
+
+    public String getForAges() {
+        return forAges;
+    }
+
+    public String[] getFormat() {
+        return format;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIllustrationsNote() {
+        return illustrationsNote;
+    }
+
+    public String getImprint() {
+        return imprint;
+    }
+
     public Date getIndexDate() {
         return indexDate;
     }
 
-    /**
-     * @param indexDate the indexDate to set
-     */
-    public void setIndexDate(Date indexDate) {
-        this.indexDate = indexDate;
+    public String getIsbn10() {
+        return isbn10;
     }
 
-    /**
-     * @return the publicationDate
-     */
+    public String getIsbn13() {
+        return isbn13;
+    }
+
+    public String[] getLang() {
+        return lang;
+    }
+
     public Date getPublicationDate() {
         return publicationDate;
     }
 
-    /**
-     * @param publicationDate the publicationDate to set
-     */
-    public void setPublicationDate(Date publicationDate) {
-        this.publicationDate = publicationDate;
+    public String getPublicationPlace() {
+        return publicationPlace;
     }
 
-    /**
-     * @return the url
-     */
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getRatingsAVG() {
+        return ratingsAVG;
+    }
+
+    public int getRatingsCount() {
+        return ratingsCount;
+    }
+
     public URL getUrl() {
         return url;
     }
 
-    /**
-     * @param url the url to set
-     */
-    public void setUrl(URL url) {
-        this.url = url;
-    }
-
-    /**
-     * @return the author
-     */
-    public List<Integer> getAuthor() {
-        return author;
-    }
-
-    /**
-     * @param author the author to set
-     */
-    public void setAuthor(List<Integer> author) {
-        this.author = author;
-    }
-
-    /**
-     * @return the format
-     */
-    public List<Integer> getFormat() {
-        return format;
-    }
-
-    /**
-     * @param format the format to set
-     */
-    public void setFormat(List<Integer> format) {
-        this.format = format;
+    public float getWeight() {
+        return weight;
     }
 }
