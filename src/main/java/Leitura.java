7733c6e9db6cd2765c3f248ee3eae1a7fb1175c5
Rodrigo Torres <rodrigotorres@ice.ufjf.br>
2020-04-04 16:46:52 -0300

    novos tratamentos para title e edition-statement

--- src/main/java/Leitura.java
+++ src/main/java/Leitura.java
@@ -108,128 +108,161 @@ public class Leitura {
     }
     // END ------------------
 
-    /**
-     * A partir do arquivo dataset a função retorna um arquivo com o atributo
-     * escolhido de cada registro, além de retornar o tamanho da maior string e sua
-     * linha correspondente
+        /**
+     * A partir do arquivo dataset a função retorna um arquivo com o atributo escolhido de cada registro,
+     * além de retornar o tamanho da maior string e sua linha correspondente
      *
      * @param file         arquivo dataset
-     * @param nomeArqSaida nome do arquivo de saida que conterá todos os registros
-     *                     do atributo escolhido
-     * @param atributo     posição correspondente ao atributo requerido, por
-     *                     exemplo, caso o atributo desejado seja authors deve
-     *                     colocar 1, categories = 3 e assim sucessivamente seguindo
-     *                     o cabeçalho do dataset
+     * @param nomeArqSaida nome do arquivo de saida que conterá todos os registros do atributo escolhido
+     * @param atributo     posição correspondente ao atributo requerido, por exemplo, caso o atributo 
+     *                     desejado seja authors deve colocar 1, categories = 3 e assim sucessivamente
+     *                     seguindo o cabeçalho do dataset
      * 
-     * @return escreve no terminal o tamanho da maior string e a linha
-     *         correspondente
+     * @return escreve no terminal o tamanho da maior string e a linha correspondente
      */
     public static void tamString(File file, String nomeArqSaida, int atributo) throws IOException {
-        BufferedReader sc = new BufferedReader(new FileReader(file));
-        FileWriter arq = new FileWriter("data/" + nomeArqSaida + ".txt");
-        String linha, aux = "", description = "";
+        BufferedReader  sc = new BufferedReader (new FileReader(file));
+        FileWriter arq = new FileWriter("data/"+nomeArqSaida+".txt");
+        String linha, aux = "", description = "", id="", title="", editionStatement="";
 
-        int quantAtributos = 0, progresso = 0, progressoAtual = 0;
+        int quantAtributos = 0;
         long quantlinhas = 0, tamMaiorString = 0, maiorLinha = 0;
-
+        
+        int progresso = 0, progressoAtual = 0;
         System.out.println(progressoAtual + "%");
 
         PrintWriter gravarArq = new PrintWriter(arq);
 
         sc.readLine(); // pular o cabeçalho
 
-        while ((linha = sc.readLine()) != null) {
+        while ( (linha = sc.readLine())!=null ) {
 
             quantlinhas++;
 
             for (int i = 0; i < linha.length(); i++) {
-
+                
                 aux += linha.charAt(i);
-
+          
                 // Caso o dado não termine na mesma linha
-                if (i == linha.length() - 1 && linha.charAt(i) != '\"') {
+                if (i == linha.length() - 1 && !linha.substring(linha.length()-2,linha.length()).matches("[0-9]\"") && !linha.substring(linha.length()-3,linha.length()).matches(",\"\"")  ) {
                     linha = sc.readLine();
-                    while (linha.length() == 0) // Caso a proxima linha seja apenas um espaço em branco encontrar uma
-                                                // linha que contenha alguma informção
+                
+                    while (linha.length() == 0 || linha.matches(" ")) //Caso a proxima linha seja apenas um espaço em branco encontrar uma linha que contenha alguma informção
                         linha = sc.readLine();
+
                     i = -1;
                 } else {
-                    if (i < linha.length() - 2) // Não se deve fazer a proxima comparação caso o caractere seja o
-                                                // penultimo ou ultimo item da linha
-                        if (linha.charAt(i) == '\"' && linha.charAt(i + 1) == ',' && linha.charAt(i + 2) == '\"') { // Verifica
-                                                                                                                    // é
-                                                                                                                    // o
-                                                                                                                    // fim
-                                                                                                                    // de
-                                                                                                                    // um
-                                                                                                                    // atributo
-                            if (aux.matches("\".*\"") || quantAtributos == 3) { //
-
+                    if (i < linha.length() - 2) //Não se deve fazer a proxima comparação caso o caractere seja o penultimo ou ultimo item da linha
+                        if (linha.charAt(i) == '\"' && linha.charAt(i + 1) == ',' && linha.charAt(i + 2) == '\"') { //Verifica é o fim de um atributo 
+                            if(aux.matches("\".*\"") || quantAtributos==3 || quantAtributos==22 || quantAtributos==8){ //
                                 quantAtributos++;
-                                // Existem registros que o atributo description possui "," no meio do dado o que
-                                // invalida a divisão de atributos
-                                // portanto, só armazena description quando o proximo atributo for um número
-                                // com um devido padrão
-                                if (quantAtributos == 4) {
-
+                                
+                                //Existem registros que o atributo description possui "," no meio do dado o que invalida a divisão de atributos
+                                //portanto, só armazena description quando o proximo atributo for um número
+                                //com um devido padrão 
+                                
+                                //Tratamento de problemas para Description 
+                                if(quantAtributos==4){
+                                  
                                     if (aux.matches(",\"[0-9]+.[0-9]+\"") || aux.matches(",\"\"")) {
                                         i -= aux.length();
                                         aux = description;
                                         description = "";
-                                    } else {
+                                    }
+                                    else{
                                         description += aux;
                                         quantAtributos--;
-
+                                      
                                     }
                                 }
+                                //END Tratamento de problemas para Description 
 
-                                if (quantAtributos == atributo && description.equals("")) { // Caso o atributo seja o
-                                                                                            // escolhido ao chamar
-                                                                                            // função
+                                //Tratamento de problemas para Title 
+                                if(quantAtributos==12)
+                                    id=aux.substring(1, aux.length()-1);
 
+                                if(quantAtributos==23){
+                                    
+                                    if (aux.matches(",\"/.*"+id+"\"") || aux.matches(",\"\"")) {
+                                        i -= aux.length();
+                                        aux = title;
+                                        title = "";
+                                    }
+                                    else{
+                                        title += aux;
+                                        quantAtributos--;
+                                      
+                                    }
+                                }
+                                //END Tratamento de problemas para Title
+
+                                //Tratamento de problemas para edition-statement 
+                                if(quantAtributos==9){
+                                    
+                                    if (aux.matches(",\"[0-9].*\"") || aux.matches(",\"\"")) {
+                                        i -= aux.length();
+                                        aux = editionStatement;
+                                        editionStatement = "";
+                                    }
+                                    else{
+                                        editionStatement += aux;
+                                        quantAtributos--;
+                                      
+                                    }
+                                }
+                                //END Tratamento de problemas para edition-statement
+
+                                if(quantAtributos==atributo && description.equals("") && title.equals("") && editionStatement.equals("")){ //Caso o atributo seja o escolhido ao chamar função 
+                                    
                                     if (tamMaiorString < aux.length()) {
                                         maiorLinha = quantlinhas;
                                         tamMaiorString = aux.length();
                                     }
-
-                                    gravarArq.printf("%s%n", quantlinhas + "." + aux.substring(1, aux.length() - 1));
+                                    
+                                    gravarArq.printf("%s%n", quantlinhas + "." + aux.substring(1, aux.length()-1) );
                                 }
-
-                                if (description.equals("")) {
-                                    i += 1; // Pula o caractere ,
+                                
+                                
+                                if(description.equals("") && title.equals("") && editionStatement.equals("")){
+                                    i += 1; //Pula o caractere ,
                                 }
 
                                 aux = "";
-
-                            }
+                                    
+                            }                           
                         }
                 }
-
-                if (i == linha.length() - 1 && atributo == 25)
-                    gravarArq.printf("%s%n", quantlinhas + "." + aux.substring(1, aux.length() - 1));
+                
+                if (i == linha.length() - 1 && atributo==25){
+                    if (tamMaiorString < aux.length()) {
+                        maiorLinha = quantlinhas;
+                        tamMaiorString = aux.length();
+                    }
+                    gravarArq.printf("%s%n", quantlinhas + "." + aux.substring(1, aux.length()-1) );
+                }
+                
             }
             aux = "";
             quantAtributos = 0;
 
-            progressoAtual = (int) (quantlinhas * 100) / 1086960; // imprimir progresso
-            if (progresso < progressoAtual) {
+            progressoAtual = (int)(quantlinhas * 100) / 1086955; //imprimir progresso
+            if(progresso < progressoAtual){
                 progresso = progressoAtual;
                 System.out.println(progressoAtual + "%");
             }
 
         }
-        System.out.println("Tamanho da maior String: " + (tamMaiorString - 2)); // -2 para não contar com as aspas no
-                                                                                // começo e final do atributo
-        System.out.println("Linha correspondente a maior string: " + maiorLinha);
-        gravarArq.printf("%s%n", "Tamanho da maior String: " + (tamMaiorString - 2));
-        gravarArq.printf("%s%n", "Linha correspondente a maior string: " + maiorLinha);
-
+        System.out.println("Tamanho da maior String: "+ (tamMaiorString-2)); //-2 para não contar com as aspas no começo e final do atributo
+        System.out.println("Linha correspondente a maior string: "+ maiorLinha);
+        gravarArq.printf("%s%n","Tamanho da maior String: "+ (tamMaiorString-2));
+        gravarArq.printf("%s%n","Linha correspondente a maior string: "+ maiorLinha);
+        
         arq.close();
-        sc.close();
-        gravarArq.close();
+        sc.close();       
     }
-
     // END ------------------
+
+
     public static void tamStringAuthors(File file, int atributo) throws IOException {
         BufferedReader sc = new BufferedReader(new FileReader(file));
         FileWriter arq = new FileWriter("data/authors.txt");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,14 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
+     
         File dataset = new File("data/dataset.csv"); 
-        Leitura.tamString(dataset,"weight",25);
+        String[] cabecalho = {"authors","bestsellers-rank","categories","description","dimension-x","dimension-y","dimension-z","edition","edition-statement","for-ages","format","id","illustrations-note","imprint","index-date","isbn10","isbn13","lang","publication-date","publication-place","rating-avg","rating-count","title","url","weight"};
+        for (int i = 1; i <= cabecalho.length; i++) {
+            Leitura.tamString(dataset,cabecalho[i-1],i);    
+        }
+        
+       
+        
     }
 }
