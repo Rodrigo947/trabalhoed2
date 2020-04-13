0ee48c58bed9ab106c0b9bec2417e5cccdfd758b
Rodrigo Torres <rodrigotorres@ice.ufjf.br>
2020-04-13 04:26:24 -0300

    exemplos de execução

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,34 @@
 import java.io.IOException;
 import Livro.Livro;
+import Ordenacao.*;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        try (BufferedReader tamanhos = new BufferedReader(new FileReader("data/tamanhos.txt")); 
+    public static void main(String[] args) throws IOException, ClassNotFoundException, CloneNotSupportedException {
+        
+        //Criando Arquivo de Objetos
+        File datasetOBJ = new File("data/datasetOBJ.txt");
+        File arqLinhas = new File("data/quantLinhas.txt");
+        if(!datasetOBJ.exists() || !arqLinhas.exists()){
+            System.out.println("Criando arquivo de Objetos....");
+            File dataset = new File("data/dataset.csv");
+            Leitura.datasetCSVtoOBJ(dataset);
+        }
+
+        
+        
+        try (BufferedReader tamanhos = new BufferedReader(new FileReader("data/entrada.txt")); 
             BufferedReader fileQuantLinhas = new BufferedReader(new FileReader("data/quantLinhas.txt"))) {
-            int quantLinhas = Integer.parseInt(fileQuantLinhas.readLine());
+            
+            int quantLinhas = Integer.parseInt(fileQuantLinhas.readLine()); //quantidade de linhas que possui o dataset
             GerarArrays gerar = new GerarArrays();
-            for (int i = 0; i < 1; i++) {
+            
+            
+            //Gera e imprime os vetores - APENAS PARA TESTE
+            for (int i = 0; i < Integer.parseInt(tamanhos.readLine()); i++) {
                 
                 //define os tamanhos dos arrays a serem criados
                 int tamanho = Integer.parseInt(tamanhos.readLine());
@@ -24,7 +42,27 @@ public class Main {
                 for (Livro livro : livros) {
                     System.out.println(livro.getId());
                 }
+                
             }
+            
+            
+            //Exemplo para rodar um algoritmo de ordenação
+            String[] titulos = gerar.arrayTitulos(1000, 123456, quantLinhas);
+            Livro[] livros = gerar.arrayLivros(1000, 654321, quantLinhas);
+
+            String[] copiaTitulos = titulos.clone();
+            Livro[] copiaLivros = new Livro[1000];
+
+            for (int i = 0; i < livros.length; i++) 
+                copiaLivros[i] = (Livro) livros[i].clone();
+            
+
+            QuickSort.QuickSortRecursivo(copiaTitulos,1000);
+            QuickSort.QuickSortRecursivo(copiaLivros,1000);
+            //END------------
+        
         }
+         
+
     }
 }
--- src/main/java/Ordenacao/QuickSort.java
+++ src/main/java/Ordenacao/QuickSort.java
@@ -1,4 +1,6 @@
 package Ordenacao;
 public class QuickSort {
-    
+    public static <T> void QuickSortRecursivo( T[] array, int tamanho){
+
+    };
 }
