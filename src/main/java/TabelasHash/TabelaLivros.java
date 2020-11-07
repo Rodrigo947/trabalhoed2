0d222ce601780e393323ebfeeebea9a49b88e945
Rodrigo Torres <rodrigotorres@ice.ufjf.br>
2020-11-07 11:24:59 -0300

    fix logica de coalecido

--- src/main/java/TabelasHash/TabelaLivros.java
+++ src/main/java/TabelasHash/TabelaLivros.java
@@ -10,18 +10,20 @@ import Livro.LivroAux;
 public class TabelaLivros {
     List<LivroAux> livros;
     List<Integer> next;
-    int tamanho;
+    int tamanho, ultimo;
 
     public TabelaLivros(int tamanho) {
         this.livros = new ArrayList<>(tamanho);
         this.next = new ArrayList<>(tamanho);
         this.tamanho = tamanho;
+        this.ultimo = tamanho-1;
         for(int i = 0; i < tamanho; i++){
             this.livros.add(new LivroAux());
             this.next.add(-2);
         }
     }
 
+    
     public void add(LivroAux livro){
                
         int posicao = Divisao(livro.getId(),tamanho);
@@ -29,11 +31,15 @@ public class TabelaLivros {
             livros.set(posicao,livro);
             next.set(posicao,-1);
         } else {
-            for(int i = tamanho-1; i >= 0; i--) {
+            while( next.get(posicao) != -1)
+                posicao = next.get(posicao);
+            
+            for(int i = ultimo; i >= 0; i--) {
                 if ( next.get(i) == -2) {
                     livros.set(i,livro);
                     next.set(i,-1);
                     next.set(posicao,i);
+                    ultimo = i-1;
                     break;
                 }
             }
